5c09a494d644bdcc45414ec693160801ddf6fc3c
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2023-11-03 15:20:47 -0400

    the chungus is amungus

--- src/main/java/com/spencer/ItemApp/models/User.java
+++ src/main/java/com/spencer/ItemApp/models/User.java
@@ -10,33 +10,38 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="users")
+@Entity(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Getter private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	private Long id;
 	private String email;
-    private String password;
-    public User(String email, String password) {
-    	this.email=email;
-    	this.password=password;
-    }
-    public User() {
-    	
-    }
-    public void setEmail(String email) {
-    	this.email = email;
-    }
-	public String getEmail() {
+	private String password;
+
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	protected User() {
 		
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+
 		return this.email;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getPassword() {
 		return this.password;
 	}
